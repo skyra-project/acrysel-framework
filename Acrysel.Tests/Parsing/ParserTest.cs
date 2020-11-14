afaa4576dd2b3078b54abaeb7f0497009bec6385
Tyler Davis <tgfdavis98@gmail.com>
2020-11-14 14:39:37 +0000

    Add some basing parsing methods, and tests.

---
+++ Acrysel.Tests/Parsing/ParserTest.cs
@@ -0,0 +1,62 @@
+﻿using Acrysel.Parsing;
+using NUnit.Framework;
+using Acrysel.Parsing;
+
+namespace Acrysel.Tests.Parsing
+{
+    [TestFixture, Parallelizable(ParallelScope.All)]
+    public class ParserTest
+    {
+        private readonly string _inputString = "!test --option=a hello 1";
+
+        [TestCase(8, 6, "option")]
+        [TestCase(0, 5, "!test")]
+        [TestCase(17, 5, "hello")]
+        [TestCase(17, 7, "hello 1")]
+        public void Parser_Peek_ReturnsCorrectSlice(int ahead, int amount, string expectedSlice)
+        {
+            // assign
+            var parser = new Parser(new[] {"!"}, " ", _inputString);
+            
+            // act
+            var slice = parser.Peek(ahead, amount);
+            
+            // assert
+            Assert.AreEqual(expectedSlice, slice);
+
+        }
+
+        [TestCase(2, 'e')]
+        [TestCase(7, '-')]
+        [TestCase(1, 't')]
+        [TestCase(0, '!')]
+        public void Parser_Consume_ReturnsCorrectChar(int ahead, char expectedValue)
+        {
+            // assign
+            var parser = new Parser(new[] {"!"}, " ", _inputString);
+            
+            // act
+            var token = parser.Consume(ahead);
+            
+            // assert
+            Assert.AreEqual(expectedValue, token);
+        }
+        
+        [TestCase(2, "e")]
+        [TestCase(7, "-")]
+        [TestCase(1, "t")]
+        [TestCase(0, "!")]
+        public void Parser_Consume_MovesHeadCorrectly(int ahead, string expectedValue)
+        {
+            // assign
+            var parser = new Parser(new[] {"!"}, " ", _inputString);
+            
+            // act
+            var _ = parser.Consume(ahead);
+            var token = parser.Peek();
+            
+            // assert
+            Assert.AreEqual(expectedValue, token);
+        }
+    }
+}
--- Acrysel/Parsing/Parser.cs
+++ Acrysel/Parsing/Parser.cs
@@ -1,7 +1,49 @@
-﻿namespace Acrysel.Parsing
+﻿using System;
+
+namespace Acrysel.Parsing
 {
-    public sealed class Parser
+    public class Parser
     {
+        private readonly string[] _prefixes;
+        private readonly string _seperator;
+        private readonly string _input;
         
+        private int _head;
+        private ParseMode _parseMode;
+        private bool _isCaseSensitive;
+
+        private readonly string[] _flagOrOptionStartHints = new[] {"--", "-", "—"};
+
+        public Parser(string[] prefixes, string seperator, string input)
+        {
+            _prefixes = prefixes;
+            _seperator = seperator;
+            _input = input;
+            _head = 0;
+            _parseMode = ParseMode.Unknown;
+        }
+
+        public string Peek(int ahead = 0, int amount = 1)
+        {
+            var start = _head + ahead;
+            var end = start + amount;
+            return _input[start..end];
+        }
+
+        public char Consume(int ahead = 0)
+        {
+            var token = _input[_head + ahead];
+            _head += ahead;
+            return token;
+        }
+        
+    }
+
+
+    public enum ParseMode
+    {
+        Flag,
+        Option,
+        Unknown
     }
 }
